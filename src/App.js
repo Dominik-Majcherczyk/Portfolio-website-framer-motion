df336dccc3c74aea68171f5bb9c5ffa2c86fabe3
Dominik-Majcherczyk <64542753+Dominik-Majcherczyk@users.noreply.github.com>
2021-03-28 12:10:10 +0200

    Global styling and about section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import AboutUs from "./pages/AboutUs";
+//global style
+import GlobalStyle from "./components/GlobalStyle";
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
