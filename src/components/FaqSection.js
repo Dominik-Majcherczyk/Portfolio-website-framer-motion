18fed6dcde3530ef69d86861f8a294622e247294
Dominik-Majcherczyk <64542753+Dominik-Majcherczyk@users.noreply.github.com>
2021-03-28 11:28:04 +0200

    Faq section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,57 @@
+import React from "react";
+import home1 from "../img/home1.png";
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions? <span>FAQ</span>
+      </h2>
+      <div className="questions">
+        <h4>question</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+            quos corrupti blanditiis nemo, quasi accusamus voluptatibus
+            praesentium voluptate cumque excepturi!
+          </p>
+        </div>
+      </div>
+      <div className="questions">
+        <h4>question</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+            quos corrupti blanditiis nemo, quasi accusamus voluptatibus
+            praesentium voluptate cumque excepturi!
+          </p>
+        </div>
+      </div>
+      <div className="questions">
+        <h4>question</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+            quos corrupti blanditiis nemo, quasi accusamus voluptatibus
+            praesentium voluptate cumque excepturi!
+          </p>
+        </div>
+      </div>
+      <div className="questions">
+        <h4>question</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+            quos corrupti blanditiis nemo, quasi accusamus voluptatibus
+            praesentium voluptate cumque excepturi!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,12 +1,14 @@
 import React from "react";
 import AboutSection from "../components/AboutSection";
 import ServicesSection from "../components/ServicesSection";
+import FaqSection from "../components/FaqSection";
 
 const AbousUs = () => {
   return (
     <div>
       <AboutSection />
       <ServicesSection />
+      <FaqSection />
     </div>
   );
 };
